e6815f26b90ff83ebf591d50eb17a3163d833997
Spacy <sylvain.venien@gmail.com>
2026-05-21 14:13:41 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -29,7 +29,23 @@ const gameSettings = {
         fontSize: "120px",
         destroyOverflowRatio: 0.2,
         minDelayNewYPos: 500,
-        minDelayLongChar: 500
+        minDelayLongChar: 500,
+    },
+    backgroundLyrics: {
+        fontSize: "500px",
+        maxDurationBGEffect: 200,
+        maxDurationBGAnim: 100,
+        sizeChangeCoeff: 5,
+        colors: ["#D00000", "#FFB703", "#2A9D8F", "#3A86FF", "#8338EC"],
+        startAlpha: 0.5
+    },
+    catchParticles: {
+        speed: { min: 200, max: 400 },
+        lifespan: { min: 300, max: 600 },
+        quantity: 10,
+        scale: {start: 0.2, end: 2 },
+        colors: [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff],
+        alpha: { min: 0.1, max: 0.6 }
     },
     charSpawnYPointer: { maxSpeed: 0.005, jerk: 0.0005 },
     minDelayLongChar: 400
@@ -98,6 +114,11 @@ class GameScene extends Phaser.Scene {
         this.fallDistance = (this.scale.width + gameSettings.lyrics.startXOffset) - this.catcherX;
         this.charSize = parseInt(gameSettings.lyrics.fontSize);
 
+        this.maxDurationBGEffect = gameSettings.backgroundLyrics.maxDurationBGEffect;
+        this.activeBGChar = null;
+        this.bgCharEndTime = 0;
+        this.bgCharColorIndex = 0;
+
         // Initialize spawn pointer: picks a Y within margins and a small random velocity
         this.charSpawnYPointer = Phaser.Math.Between(gameSettings.lyrics.marginY, this.scale.width - gameSettings.lyrics.marginY);
         this.charSpawnYPointerAccelleration = Phaser.Math.FloatBetween(- gameSettings.charSpawnYPointer.maxSpeed / 2, gameSettings.charSpawnYPointer.maxSpeed / 2);
@@ -115,6 +136,31 @@ class GameScene extends Phaser.Scene {
                 this.timeText.setText(taPlayer.timer.position.toString());
             }
         });
+
+        // Create the background char that gets updated when you catch one
+        this.activeBGChar = this.add.text(this.scale.width / 2, this.scale.height / 2, "", {
+            fontFamily: gameSettings.fonts.main,
+            fontSize: gameSettings.backgroundLyrics.fontSize,
+            color: gameSettings.backgroundLyrics.colors[this.bgCharColorIndex]
+        }).setOrigin(0.5).setAlpha(0).setDepth(-1);
+
+        // Create particle effect for when you catch a char
+        const particleGraphics = this.make.graphics({ x: 0, y: 0, add: false });
+        particleGraphics.fillStyle(0xffffff, 1);
+        particleGraphics.fillRect(0, 0, 8, 8);
+        particleGraphics.generateTexture("particle", 8, 8);
+        particleGraphics.destroy();
+
+        this.catchParticles = this.add.particles(0, 0, "particle", {
+            speed: gameSettings.catchParticles.speed,
+            angle: { min: -30, max: 30 },
+            scale: gameSettings.catchParticles.scale,
+            lifespan: gameSettings.catchParticles.lifespan,
+            quantity: gameSettings.catchParticles.quantity,
+            emitting: false,
+            tint: gameSettings.catchParticles.colors,
+            alpha: gameSettings.catchParticles.alpha
+        });
     }
 
     update(time, delta) {
@@ -128,6 +174,7 @@ class GameScene extends Phaser.Scene {
         this.spawnPendingChars(songTime, startX, w, h);
         this.updateActiveChars(songTime, startX);
         this.destroyStrips(delta);
+        this.updateBGChar();
     }
 
     handleInput(sceneHeight) {
@@ -160,7 +207,7 @@ class GameScene extends Phaser.Scene {
         strip = this.add.rectangle(
             startX,
             yPos,
-            length * 0.7,
+            length,
             height,
             0x000000,
             1
@@ -185,7 +232,7 @@ class GameScene extends Phaser.Scene {
                 }).setOrigin(0.5);
 
                 console.log(`Spawning char '${nextChar.text}' at time ${time}ms (scheduled for ${nextChar.startTime}ms), falltime ${this.fallTime}ms, startX ${startX}, spawnY ${this.charSpawnYPointer}`);
-                const charStripRef = this.spawnStrip(startX + this.charSize/2, yPos, nextChar.endTime - nextChar.startTime - this.charSize * 1.5, this.charSize/5);
+                const charStripRef = this.spawnStrip(startX + this.charSize/2, yPos, nextChar.endTime - nextChar.startTime - this.charSize, this.charSize/5);
                 
                 this.charGroup.add(charObj);
                 this.activeChars.push({ char: nextChar, obj: charObj, strip: charStripRef });
@@ -197,6 +244,7 @@ class GameScene extends Phaser.Scene {
     }
 
     destroyStrips(delta){
+        // Lower progressively the size of each strip
         for (let i = this.dyingStrips.length - 1; i >= 0; i--){
             const item = this.dyingStrips[i];
 
@@ -237,8 +285,10 @@ class GameScene extends Phaser.Scene {
             }
         }
     }
-
+    
     catchChar(catcher, charObj) {
+        this.catchParticles.emitParticleAt(this.catcher.x, this.catcher.y, 20);
+
         const idx = this.activeChars.findIndex(l => l.obj === charObj);
         if (idx > -1) {
             const char = this.activeChars[idx];
@@ -247,11 +297,49 @@ class GameScene extends Phaser.Scene {
                 charObj.destroy();
             }
             else{
+                // Handle the strip if there is one
                 const strip = char.strip;
                 const stripDuration = (strip.width / this.fallDistance) * this.fallTime;
                 this.dyingStrips.push({ strip: strip, maxWidth: strip.width, progress: 0, obj: charObj, stripDuration: stripDuration });
                 this.catcherCurrentSpeed = 0.5;
             }
+
+            // Update background character text
+            const charDuration = char.char.endTime - char.char.startTime;
+            this.bgCharStartTime = taPlayer.timer.position;
+            this.bgCharEndTime = taPlayer.timer.position + charDuration;
+
+            this.activeBGChar.setText(charObj.text);
+            this.activeBGChar.setAlpha(gameSettings.backgroundLyrics.startAlpha);
+
+            this.bgCharColorIndex = (this.bgCharColorIndex + 1) % gameSettings.backgroundLyrics.colors.length;
+            const color = gameSettings.backgroundLyrics.colors[Math.floor(Math.random() * gameSettings.backgroundLyrics.colors.length)];
+            this.activeBGChar.setColor(color);
+            this.activeBGChar.setFontSize(gameSettings.backgroundLyrics.fontSize);
+
+            window.navigator.vibrate(charDuration);
+        }
+    }
+
+    updateBGChar(){
+        if (!this.activeBGChar){
+            return;
+        }
+
+        const animTime = gameSettings.backgroundLyrics.maxDurationBGAnim;
+        const position = taPlayer.timer.position;
+
+        if (position >= this.bgCharEndTime){
+            if (this.activeBGChar.alpha > 0){
+                this.activeBGChar.setAlpha(this.activeBGChar.alpha - 1/this.maxDurationBGEffect);
+            }
+            else{
+                this.activeBGChar.setText(null);
+            }
+        }
+        else if (position <= this.bgCharStartTime + animTime){
+            const currentSize = parseInt(this.activeBGChar.style.fontSize);
+            this.activeBGChar.setFontSize(`${currentSize + gameSettings.backgroundLyrics.sizeChangeCoeff}px`);
         }
     }
 
